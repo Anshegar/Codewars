15fa768d6bfb8bda88717d3c9ca4d4452fb63ef8
Anshegar <mtxvoron@mail.ru>
2024-05-07 10:49:18 +0300

    + Binary Tree Zigzag Level Order Traversal - auto list activate

--- Binary_Tree_Zigzag_Level_Order_Traversal.py
+++ Binary_Tree_Zigzag_Level_Order_Traversal.py
@@ -1,4 +1,6 @@
 '''
+Binary Tree Zigzag Level Order Traversal
+
 Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. 
 (i.e., from left to right, then right to left for the next level and alternate between).
 
@@ -13,20 +15,28 @@ The number of nodes in the tree is in the range [0, 2000].
 значения узлов в "зигзагообразном" порядке. Это означает, что на первом уровне значения выводятся слева направо, 
 на втором уровне - справа налево, на третьем - снова слева направо, и так далее, чередуя направление обхода.
 '''
-
+from typing import List, Optional 
+from collections import deque
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:     
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+        
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         # Если корень дерева равен None, возвращаем пустой список
         if not root:
             return []
         
+        # Класс deque (от англ. "double-ended queue") - это двусторонняя очередь, 
+        # которая позволяет добавлять и извлекать элементы с обоих концов. 
+        # Он используется в коде для обхода бинарного дерева в уровень, 
+        # чтобы получить результат в виде списка списков, где каждый список содержит значения узлов 
+        # на соответствующем уровне.
         # Инициализируем очередь с корневым узлом
         queue = deque([root])
         # Инициализируем список для хранения результата
@@ -65,3 +75,27 @@ class Solution:
         
         # Возвращаем результат
         return result
+        
+s = Solution()
+
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+print(s.zigzagLevelOrder(root))
+
+def list_to_tree(nodes, index=0):
+    if index < len(nodes):
+        if nodes[index] is None:
+            return None
+        root = TreeNode(nodes[index])
+        root.left = list_to_tree(nodes, 2 * index + 1)
+        root.right = list_to_tree(nodes, 2 * index + 2)
+        return root
+
+
+root_auto_val = [3,9,20,None,None,15,7]
+root_auto = list_to_tree(root_auto_val)
+print(s.zigzagLevelOrder(root_auto))
